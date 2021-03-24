a817fda0465f897a7c049ebf079250af96f17f62
thomas lincoln <thmas5952@gmail.com>
2021-03-24 13:48:35 -0300

    Adição do Js e atualização do estilo

---
+++ app.js
@@ -0,0 +1,22 @@
+var captura = "";
+var resposta = "";
+var img = document.getElementById("imagem");
+
+function capturaResposta() {
+  captura = document.getElementById("resposta").value;
+  resposta = captura;
+}
+
+function brilho() {
+  document.getElementById("imagem").style.filter = "brightness(1)";
+}
+
+function mostra() {
+  capturaResposta();
+  if (resposta == "rafachu") {
+    alert("parabéns, você desbloqueou a skin rara da Rafa, a rafachu!");
+    brilho();
+  } else {
+    alert("Você errou, continue tentando!");
+  }
+}
